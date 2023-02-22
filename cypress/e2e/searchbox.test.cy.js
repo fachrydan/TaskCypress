7829f8e3773da5ed9c1b22e0d0de2f6321ec2ef7
Muhammad Fachry Ramadhan <78468976+fachrydan@users.noreply.github.com>
2023-02-22 13:18:58 +0700

    publish task

---
+++ cypress/e2e/searchbox.test.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Searchbox Test', function(){
+
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+    })
+
+    it('Should type into searchbox and submit', () => {
+        cy.get ('#searchTerm').type('Online {enter}')
+
+        //cy.get ('h2').should('contain.text', 'Search Results:')
+
+    })
+
+    it('Should show search result page "online"', () => {
+        cy.visit('http://zero.webappsecurity.com/search.html?searchTerm=online')
+    
+        cy.get ('h2').should('contain.text', 'Search Results:')
+
+        cy.get ('a').should('contain.text', 'Free Access to Online Banking')
+        cy.get ('a').should('contain.text', 'Online Statements')
+    })
+});
